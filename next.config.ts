8bc7e76fcc6ac575a46762b544e7dd4b973c5488
ziadelfeky404-code <ziadelfeky404@gmail.com>
2026-04-13 01:34:18 +0200

    Add logo image from Google Photos

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
